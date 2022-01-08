7dcd04b92a66e16965cd2b10961008d8c1763f25
Alexander Tretyakov <tpetyakov@gmail.com>
2022-01-08 20:38:34 +0300

    init: start js30 random-game: minesweeper

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/index.scss';
+
+// Easy 9-9 (10 mines), Medium 16x16 (40 mines), Expert 30x16 (99 mines)
+// UI -
+// timer (click on timer - ask to reset a game),
+// number of mines,
+// menu (difficulty, sound volume?),
+// sounds?
